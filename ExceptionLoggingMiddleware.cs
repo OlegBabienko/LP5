f75fbc76371446d4eed84af6110ab9c22fcd4f9b
OlegBabienko <122570232+OlegBabienko@users.noreply.github.com>
2024-10-14 22:40:41 +0300

    Добавьте файлы проекта.

---
+++ ExceptionLoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using lr5.Services; 
+
+namespace Lr5Project.Middlewares
+{
+    public class ExceptionLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context); // Передаємо запит далі по конвеєру
+            }
+            catch (Exception ex)
+            {
+                // Логування виключення в файл
+                string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "logs", "exceptions.txt");
+                await File.AppendAllTextAsync(logFilePath, $"{DateTime.Now}: {ex.Message}\n");
+                throw; // Перевикидаємо виключення далі
+            }
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lr5.Services;
+
+namespace Lr5Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(string value, DateTime expiryDate)
+        {
+            // Записуємо значення в Cookies з вказаною датою старіння
+            CookieOptions options = new CookieOptions
+            {
+                Expires = expiryDate
+            };
+            Response.Cookies.Append("CustomValue", value, options);
+
+            return RedirectToAction("CheckCookie");
+        }
+
+        public IActionResult CheckCookie()
+        {
+            // Перевіряємо наявність значення в Cookies
+            string cookieValue = Request.Cookies["CustomValue"];
+            if (cookieValue != null)
+            {
+                ViewBag.Value = cookieValue;
+                return View("CookieFound");
+            }
+            return View("CookieNotFound");
+        }
+    }
+}
